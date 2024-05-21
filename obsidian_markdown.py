c7dcc1a3e101ed7e4395ace471534ca28e324d97
BITniki <shakarimovdima@ya.ru>
2024-05-21 12:22:01 +0300

    init

---
+++ obsidian_markdown.py
@@ -0,0 +1,37 @@
+"""Facade for markdown.Markdown class.
+Provides a Obsidian like configured markdown.Markdown class.
+
+GitHub: https://github.com/BlTniki/python-obsidian-like-markdown-renderer
+"""
+from markdown import Markdown
+
+default_extensions = []
+default_extension_configs = {}
+
+def ObsidianMarkdown(**kwargs):
+    """
+    Creates a new Markdown instance.
+    Simply wraps markdown.Markdown providing kwargs directly to the underlying Markdown class.
+    if extensions or extension_configs are not provided, they will be set to the default values.
+
+    Keyword Arguments:
+        extensions (list[Extension | str]): A list of extensions.
+
+            If an item is an instance of a subclass of [`markdown.extensions.Extension`][],
+            the instance will be used as-is. If an item is of type `str`, it is passed
+            to [`build_extension`][markdown.Markdown.build_extension] with its corresponding
+            `extension_configs` and the returned instance  of [`markdown.extensions.Extension`][]
+            is used.
+        extension_configs (dict[str, dict[str, Any]]): Configuration settings for extensions.
+        output_format (str): Format of output. Supported formats are:
+
+            * `xhtml`: Outputs XHTML style tags. Default.
+            * `html`: Outputs HTML style tags.
+        tab_length (int): Length of tabs in the source. Default: `4`
+
+    """
+    if "extensions" not in kwargs:
+        kwargs["extensions"] = default_extensions
+    if "extension_configs" not in kwargs:
+        kwargs["extension_configs"] = default_extension_configs
+    return Markdown(**kwargs)
